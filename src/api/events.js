cf0d7782b25b350351f561a2a3747b399c1794cf
fastam <mikenin.d.a@gmail.com>
2026-04-24 20:48:25 +0300

    bitrix

--- src/api/events.js
+++ src/api/events.js
@@ -1,85 +1,21 @@
-const BASE =
-'/bitrix/services/main/ajax.php?action=vendor:events.event.';
-
-
-function mapEvent(e){
-
-  return {
-
-    id:e.id,
-
-    title:e.name,
-
-    description:e.preview,
-
-    fullDescription:e.text || e.preview,
-
-    date:e.date,
-
-    image:'https://picsum.photos/600/400?'+e.id,
-
-    location:'online',
-
-    organizer:'Bitrix',
-
-    participants:100,
-
-    requirements:'без требований',
-
-    tags:e.tags || []
-
-  }
-
+async function bxRequest(actionName, data = {}) {
+  return new Promise((resolve, reject) => {
+    window.BX.ajax.runAction(`zlata:events.event.${actionName}`, {
+      data: data
+    })
+    .then(response => resolve(response.data))
+    .catch(error => reject(error));
+  });
 }
 
-
-
-export async function getEvents(page = 1, tags = []){
-
-  let url =
-  BASE + 'list&page=' + page;
-
-
-  if(tags.length){
-
-    url += '&tag=' + tags.join(',');
-
-  }
-
-
-  let r =
-  await fetch(url)
-  .then(r => r.json())
-
-
-  return r.data.items.map(mapEvent)
-
+export async function getEvents(page = 1, tag = null) {
+  return await bxRequest('list', { page, tag });
 }
 
-
-
-export async function getEvent(id){
-
-  let r =
-  await fetch(
-    BASE + 'detail&id=' + id
-  ).then(r => r.json())
-
-
-  return mapEvent(r.data)
-
+export async function getEvent(id) {
+  return await bxRequest('detail', { id });
 }
 
-
-
-export async function getTags(){
-
-  let r =
-  await fetch(
-    BASE + 'tags'
-  ).then(r => r.json())
-
-
-  return r.data
-
+export async function getTags() {
+  return await bxRequest('tags');
 }
